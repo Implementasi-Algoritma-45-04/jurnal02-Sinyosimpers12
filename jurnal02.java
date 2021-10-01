1d3ffe27d197a454f9267d7c1fd5c24d043bc9db
Sinyosimpers12 <91170198+Sinyosimpers12@users.noreply.github.com>
2021-10-01 15:43:58 +0800

    Solusi Jurnal02

--- jurnal02.java
+++ jurnal02.java
@@ -1,7 +1,7 @@
-public class jurnal02{
+public class Jurnal02{
     public static void main(String[] args){
         System.out.println("Nama :Sinyo Simpers Soba");
-         System.out.println("Nim :6706213075");
-          System.out.println("Kelas :D3RPLA04");
+        System.out.println("Nim :6706213075");
+        System.out.println("Kelas :D3RPLA04");
     }
 }
